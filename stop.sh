4826685e9b176997920c626be95f5f0cfe39867b
Micha2536 <43035254+Micha2536@users.noreply.github.com>
2026-03-15 15:32:36 +0100

    Add support to stop an additional Docker container

--- stop.sh
+++ stop.sh
@@ -2,3 +2,5 @@
 set -eu
 CONTAINER_NAME="${CONTAINER_NAME:-vhih-hue-modul}"
 docker stop "$CONTAINER_NAME" 2>/dev/null || true
+CONTAINER_NAME2="${CONTAINER_NAME2:-vhih-mqtt-broker}"
+docker stop "$CONTAINER_NAME2" 2>/dev/null || true
